17a8a697e0a076de08cbc67431cb97b50fb0e617
cjyuan <cjyuan00.gmail.com>
2026-05-23 23:19:06 +0100

    Updated ITD to include Surveys

---
+++ custom-scripts/reviews/common.mjs
@@ -0,0 +1,115 @@
+function daysToMilliseconds(days) {
+    return days * 24 * 60 * 60 * 1000;
+}
+
+function identifyAge(date) {
+    const millis = new Date() - date;
+    if (millis < daysToMilliseconds(7)) {
+        return "this week";
+    } else if (millis < daysToMilliseconds(7 * 4)) {
+        return "this month";
+    } else {
+        return "old";
+    }
+}
+
+const apiPrefix = "https://github-issue-proxy.hosting.codeyourfuture.io/cached/2/repos/CodeYourFuture";
+
+// PR Title format is 1 Location 2 Cohort 3 Name 4 Sprint # 5 taskname
+const prTitleParse = new RegExp(/.+\|.+\|.+\|[\D]+?(\d)\s*\|\s*(.+)/i);
+const prTaskNameCleaner = new RegExp(/\W|sprints?|courseworks?|exercises?|tasks?/gi);
+
+class PR {
+    // status: one of: "Needs Review", "Reviewed", "Complete", "Closed", "Unknown"
+    constructor(url, number, userName, userUrl, title, module, createdAge, updatedAge, status) {
+        this.url = url;
+        this.number = number;
+        this.userName = userName;
+        this.userUrl = userUrl;
+        this.title = title;
+        this.module = module;
+        this.createdAge = createdAge;
+        this.updatedAge = updatedAge;
+        this.status = status;
+        this._didLoadReviews = false;
+        this.reviews = [];
+        try {
+            const parsedTitle = title.match(prTitleParse);
+            this.sprint = parseInt(parsedTitle[1]);
+            this.taskName = parsedTitle[2].toLowerCase().replaceAll(prTaskNameCleaner, "");
+        } catch (e) {
+            console.warn("failed to properly parse ", this.title, this.url);
+            this.sprint = 'unknown';
+            this.taskName = 'unknown';
+        }
+    }
+
+    async loadReviews() {
+        if (this._didLoadReviews) {
+            return;
+        }
+        const response = await fetch(`${apiPrefix}/${this.module}/pulls/${this.number}/reviews`);
+        const reviews = await response.json();
+        for (const reviewResponse of reviews) {
+            const review = new Review(reviewResponse.user.login, reviewResponse.user.login === this.userName, identifyAge(new Date(Date.parse(reviewResponse["submitted_at"]))), reviewResponse.state);
+            this.reviews.push(review);
+        }
+        this._didLoadReviews = true;
+    }
+
+    hasReviewer() {
+        return this.reviews.some(review => !review.isPrAuthor);
+    }
+}
+
+class Review {
+    constructor(userName, isPrAuthor, age, state) {
+        this.userName = userName;
+        this.isPrAuthor = isPrAuthor;
+        this.age = age;
+        // e.g. "COMMENTED".
+        this.state = state;
+    }
+}
+
+class Comment {
+    constructor(userName, isPrAuthor, createdAt) {
+        this.userName = userName;
+        this.isPrAuthor = isPrAuthor;
+        this.createdAt = createdAt;
+    }
+}
+
+function getStatus(state, labels) {
+    // TODO: Check possibilities
+    if (state !== "open") {
+        return "Closed"
+    }
+    for (const possibleLabel of ["Needs Review", "Complete", "Reviewed"]) {
+        if (labels.some((label) => label.name === possibleLabel)) {
+            return possibleLabel;
+        }
+    }
+    return "Unknown";
+}
+
+async function fetchPrsWithoutLoadingReviews() {
+    const prs = [];
+    const responsePromises = [];
+    for (const module of modules) {
+        responsePromises.push(fetch(`${apiPrefix}/${module}/pulls`).then((response) => response.json()));
+    }
+    const responsesByModule = await Promise.all(responsePromises);
+    for (let i = 0; i < responsesByModule.length; i++) {
+        const module = modules[i];
+        const responsePrs = responsesByModule[i];
+        for (const pr of responsePrs) {
+            const status = getStatus(pr.state, pr.labels);
+            const createdAt = new Date(Date.parse(pr["created_at"]));
+            const updatedAt = new Date(Date.parse(pr["updated_at"]));
+
+            prs.push(new PR(pr.html_url, pr.number, pr.user.login, pr.user.html_url, pr.title, module, identifyAge(createdAt), identifyAge(updatedAt), status));
+        }
+    }
+    return prs;
+}
---
+++ custom-scripts/reviews/index.mjs
@@ -0,0 +1,267 @@
+// 400/700/900
+function badness(name, count) {
+  if ((name === "old" || name == "this month") && count > 0) {
+    return 900;
+  }
+  if (name === "this week" && count > 20) {
+    return 900;
+  }
+  if (name === "this week" && count > 10) {
+    return 700;
+  }
+  return 400;
+}
+
+const ageToEmoji = {
+  "this week": "🟢",
+  "this month": "🟠",
+  old: "🔴",
+};
+
+function computeStatusClass(awaitingReview) {
+  const score = Math.max(
+    ...Object.entries(awaitingReview).map(([name, count]) =>
+      badness(name, count)
+    )
+  );
+  if (score === 900) {
+    return "is-bad";
+  } else if (score === 700) {
+    return "is-medium";
+  } else {
+    return "is-good";
+  }
+}
+
+const state = {
+  "prs": null,
+  "reviewer_filter": "",
+  "region_filter": "",
+}
+
+async function onLoad() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const region = urlParams.get("region");
+  state.region_filter = region;
+
+  render();
+  state.prs = (await fetchPrsWithoutLoadingReviews()).filter((pr) => pr.status === "Needs Review");
+  render();
+  await Promise.all(state.prs.map((pr) => pr.loadReviews()));
+  render();
+
+}
+
+function render() {
+  // Create regions filter object
+  const regionSelect = document.querySelector("#region-filter");
+  regionSelect.textContent = "";
+  regionSelect.appendChild(makeRegionOption("All Regions", ""));
+  for (const region of Object.keys(regionAliases)) {
+    regionSelect.appendChild(makeRegionOption(region, region));
+  }
+  regionSelect.appendChild(makeRegionOption("Unknown", "Unknown"));
+  regionSelect.addEventListener("change", (event) => {
+    state.region_filter = event.target.value;
+    render();
+  });
+
+  // Default loading state
+  if (state.prs === null) {
+    document.querySelector("#pr-list").innerText = "Loading...";
+    return;
+  }
+
+  const awaitingReviewByAge = {};
+  const prsByModule = {};
+  const reviewers = new Set();
+
+  for (const module of modules) {
+    awaitingReviewByAge[module] = {
+      "this week": 0,
+      "this month": 0,
+      old: 0,
+    };
+    prsByModule[module] = [];
+  }
+
+  // Filter all the relevant PRs to show, sorted by oldest first
+  for (const pr of state.prs) {
+    if (state.region_filter && !regionMatches(state.region_filter, pr.title)) {
+      continue;
+    }
+    for (const review of pr.reviews) {
+      if (!review.isPrAuthor) {
+        reviewers.add(review.userName);
+      }
+    }
+    if (state.reviewer_filter && !pr.reviews.some((review) => review.userName.toLowerCase().startsWith(state.reviewer_filter.toLowerCase()))) {
+      continue;
+    }
+
+    awaitingReviewByAge[pr.module][pr.updatedAge]++;
+    prsByModule[pr.module].push(pr);
+  }
+  for (const module of modules) {
+    prsByModule[module].sort((l, r) => {
+      if (l.updatedAge > r.updatedAge) {
+        return 1;
+      } else if (l.updatedAge < r.updatedAge) {
+        return -1;
+      } else {
+        return l.number - r.number;
+      }
+    });
+  }
+
+  document.querySelector("#pr-list").innerText = "";
+  document.querySelector("#overview").innerText = "";
+
+  // Build each module
+  for (const module of modules) {
+    const awaitingReview = awaitingReviewByAge[module];
+    const totalPending = Object.values(awaitingReview).reduce(
+      (acc, cur) => acc + cur,
+      0
+    );
+
+    // Create a traffic light card to show at top of page
+    const overviewCard = document
+      .querySelector("template.overview-card")
+      .content.cloneNode(true);
+    fillWithModuleHeading(overviewCard.querySelector(".module"), module, totalPending);
+    for (const [age, count] of Object.entries(awaitingReview)) {
+      const bucket = overviewCard.querySelector(
+        `.age-bucket.${age.replaceAll(" ", "-")} + .count`
+      );
+      if (bucket) bucket.innerText = count;
+      if (bucket && badness(age, count) === 900) {
+        bucket.classList.add("is-problem");
+      }
+    }
+    overviewCard
+      .querySelector(".overview-card")
+      .classList.add(computeStatusClass(awaitingReview));
+    document.querySelector("#overview").appendChild(overviewCard);
+
+    // Add the PRs for each module
+    if (totalPending) {
+      const modulePrList = document
+        .querySelector("template.pr-list")
+        .content.cloneNode(true);
+      fillWithModuleHeading(modulePrList.querySelector(".module"), module, totalPending);
+
+      const prsBySprint = {};
+      prsByModule[module].forEach(pr => {
+        if(!(pr.sprint in prsBySprint)){
+          prsBySprint[pr.sprint] = [];
+        }
+        prsBySprint[pr.sprint].push(pr);
+      });
+
+      for (const sprint in prsBySprint){
+        const sprintHeading = document.createElement('h3');
+        sprintHeading.innerText = "Sprint " + sprint;
+        modulePrList.querySelector("ul.pr-list").appendChild(sprintHeading);
+
+        const prsInSprint = prsBySprint[sprint];
+        prsInSprint.sort((a,b) => a.taskName < b.taskName ? -1 : 1);
+
+        for (const pr of prsInSprint) {
+          const prInList = document
+            .querySelector("template.pr-in-list")
+            .content.cloneNode(true);
+
+          const emojiElement = prInList.querySelector(".emoji");
+          if (pr.hasReviewer()) {
+            emojiElement.innerText = "🙋🏾";
+            const reviewers = [...new Set(pr.reviews.filter((reviewer) => !reviewer.isPrAuthor).map((reviewer) => reviewer.userName))];
+            const maybeS = reviewers.length === 1 ? "" : "s";
+            emojiElement.title = `Has reviewer${maybeS}: ${reviewers.join(", ")}`;
+          } else {
+            emojiElement.innerText = ageToEmoji[pr.updatedAge];
+          }
+
+          const prLink = prInList.querySelector("a.pr-link");
+          prLink.href = pr.url;
+          prLink.innerText = `${pr.title}`;
+
+          const userLink = prInList.querySelector("a.user-link");
+          userLink.href = pr.userUrl;
+          userLink.innerText = `${pr.userName}`;
+
+          prInList.querySelector(".pr-number").innerText = pr.number;
+          modulePrList.querySelector("ul.pr-list").appendChild(prInList);
+        }
+      }
+      document.querySelector("#pr-list").appendChild(modulePrList);
+    }
+  }
+
+  // Set up the filter for reviewers
+  const knownReviewersElement = document.querySelector("#known-reviewers");
+  knownReviewersElement.innerText = "";
+  const sortedReviewers = [...reviewers].sort();
+  for (const reviewer of sortedReviewers) {
+    const option = document.createElement("option");
+    option.value = reviewer;
+    knownReviewersElement.appendChild(option);
+  }
+
+}
+
+const regionMatches = (regionToFilter, prTitle) => {
+  if (regionToFilter == "Unknown") {
+    for (const region of Object.keys(regionAliases)) {
+      if (regionMatches(region, prTitle)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  const lowerParts = prTitle.toLowerCase().split("|").map((part) => part.trim());
+  for (const alias of [regionToFilter, ...regionAliases[regionToFilter]]) {
+    const lowerAlias = alias.toLowerCase();
+    for (const lowerPart of lowerParts) {
+      if (lowerPart == lowerAlias) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+const regionAliases = {
+  "Cape Town": ["South Africa", "SouthAfrica", "ZA", "ZA Cape Town"],
+  "Glasgow": ["Scotland"],
+  "London": [],
+  "North West": ["NW", "Manchester"],
+  "Sheffield": [],
+  "West Midlands": ["WM", "WestMidlands", "West-Midlands"],
+}
+
+const fillWithModuleHeading = (container, module, pending) => {
+  container.innerText = "";
+  const link = document.createElement("a");
+  link.innerText = module;
+  link.href = `https://github.com/CodeYourFuture/${module}/pulls`;
+  const text = document.createTextNode(` (${pending})`);
+  container.append(link, text);
+}
+
+const makeRegionOption = (label, value) => {
+  const option = document.createElement("option");
+  option.textContent = label;
+  option.value = value;
+  option.selected = state.region_filter === value;
+  return option;
+}
+
+onLoad();
+
+// TODO add a filter for task
+document.querySelector("#reviewer-filter").addEventListener("keyup", (event) => {
+  state.reviewer_filter = event.target.value;
+  console.log("Setting filter to " + state.reviewer_filter);
+  render();
+});
